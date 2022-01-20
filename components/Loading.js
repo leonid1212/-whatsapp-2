1eaaf29bc8ef5e355ad443cd7de737219e493656
leonid <leonid.notik.work@gmail.com>
2022-01-20 22:44:02 +0200

    main the messaging part is finished

--- components/Loading.js
+++ components/Loading.js
@@ -1,24 +1,21 @@
 
 import { Rings } from 'react-loader-spinner';
 import Image from 'next/image';
+import whatsappPic from '../public/whatsapp.png';
 
 
 function Loading() {
- 
-
-
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <Image
-          src={"/whatsapp.png"}
-          alt="place"
-          style={{ marginBottom: 10 }}
-          height={200}
-          width={200}
-          
-        
-        />
+        <div style={{ marginBottom: 10 }}>
+          <Image
+            src={ whatsappPic}
+            alt="place"
+            height={200}
+            width={200}
+          />
+        </div>
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
           <Rings
             heigth="100"
--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,9 @@ import Head from 'next/head';
 import styled from 'styled-components';
 import { Button } from "@material-ui/core";
 import { auth, provider, signInWithPopup, GoogleAuthProvider } from "../firebase";
+import whatsappPic from '../public/whatsapp.png';
+import Image from 'next/image';
+
 
 function Login() {
 
@@ -9,7 +12,7 @@ function Login() {
     signInWithPopup(auth, provider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential =GoogleAuthProvider.credentialFromResult(result);
+        const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
         // The signed-in user info.
         const user = result.user;
@@ -22,7 +25,7 @@ function Login() {
         const email = error.email;
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
-       
+
       });
   }
 
@@ -33,7 +36,13 @@ function Login() {
         <title>Login</title>
       </Head>
       <LoginContainer>
-        <Logo src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" />
+        <Logo>
+          <Image src={whatsappPic}
+            height={200}
+            width={200}
+          />
+
+        </Logo>
         <Button onClick={signIn} variant="outlined">Sign in with Google</Button>
       </LoginContainer>
     </Container>
@@ -58,9 +67,8 @@ const LoginContainer = styled.div`
   border-radius: 5px;
   box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
 `;
-const Logo = styled.img`
-  height : 200px;
-  width : 200px;
+const Logo = styled.div`
+ 
   margin-bottom : 50px;
 
 `;
